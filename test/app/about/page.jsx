17b0149b086cbd5f4cef98d130d255aff01317a6
Dion Ang <dion@Dions-MacBook-Pro.local>
2023-03-01 23:07:54 +0800

    18:15 nextjs course

---
+++ test/app/about/page.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <div>
+      <h1>ABOUT PAGE</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+        eveniet commodi ex architecto ad illo minus enim veniam autem. Velit
+        perferendis repellat officia iusto rem odit repellendus, fugiat dolorem
+        labore.
+      </p>
+      <Link href="/">Home Page</Link>
+    </div>
+  );
+}
